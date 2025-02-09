9ea68b5a5a8758fb3ce343f26b6572a226b7f01d
OmarAwadSaber <01004005001a@gmail.com>
2025-02-10 00:29:32 +0200

    models added

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Recipe, RecipeIngredients, RecipeNutrition, RecipeVersions, Ingredient, Comments
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredients)
+admin.site.register(RecipeNutrition)
+admin.site.register(RecipeVersions)
+admin.site.register(Ingredient)
+admin.site.register(Comments)
