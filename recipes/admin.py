0b9ba79c723466643b3f5a05a29448ac12fcb0b3
OmarAwadSaber <01004005001a@gmail.com>
2025-02-19 04:56:48 +0200

    modified

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Recipe, RecipeNutrition, RecipeVersions, Ingredient, Comments
 # Register your models here.
 admin.site.register(Recipe)
-# admin.site.register(RecipeIngredients)
 admin.site.register(RecipeNutrition)
 admin.site.register(RecipeVersions)
 admin.site.register(Ingredient)
